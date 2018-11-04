f5f574d1e6d65f53e5ab578edfaee210583de39d
Sharon Mun <sharonymun@gmail.com>
2018-11-04 02:44:53 +0000

    automatically backed up by learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -23,12 +23,15 @@ class MusicLibraryController
     when "list songs"
       list_songs
     when "list artists"
-      
-    
+      list_artists
     when "list genres"
+      list_genres
     when "list artist"
+      list_songs_by_artist
     when "list genre"
+      list_songs_by_genre
     when "play song"
+      play_song
   end
   end
   
